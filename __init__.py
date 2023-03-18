0fa06104e0ebfa041bf682c4c197d446806461ff
Rvosuke <2409215698@qq.com>
2023-03-19 00:18:10 +0800

    3.19

--- __init__.py
+++ __init__.py
@@ -1,4 +1,18 @@
-list1 = ['a', 'b']
-list2 = ['c', 'd']
-list1.extend(list2)
-print(list1)
+from PIL import Image, ImageDraw
+
+# Create a new RGBA image
+img = Image.new("RGBA", (512, 512), (255, 255, 255, 255))
+
+# Create a new ImageDraw object
+draw = ImageDraw.Draw(img)
+
+# # Draw a filled polygon on the image
+# points = [(100, 100), (100, 400), (400, 400), (400, 100)]
+# draw.polygon(points, fill=(0, 255, 0, 128))
+triangle = Image.new('RGBA', (512, 512), (255, 255, 255, 255))
+draw2 = ImageDraw.Draw(triangle)
+draw2.polygon([(10, 10), (50, 10), (10, 50)], fill=(0, 0, 255, 128))
+
+img = Image.alpha_composite(img, triangle)
+# Display the image
+img.show()
--- imageFitting.py
+++ imageFitting.py
@@ -1,18 +1,37 @@
 import numpy as np
 import random
-import cv2
+# import cv2
+from PIL import Image, ImageDraw
+
 
 # Parameters
-population_size = 100
-num_triangles = 50
+population_size = 20
+num_triangles = 128
 mutation_rate = 0.1
 crossover_rate = 0.8
-num_generations = 1000
+num_generations = 100
+
+
+# def load_target_image(image_path):
+#     # Load the target image using OpenCV
+#     target_image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
+#
+#     # Convert the target image to floating point representation in the range [0, 1]
+#     target_image = target_image.astype(np.float32) / 255.0
+#
+#     return target_image
 
 
 def load_target_image(image_path):
-    # Load the target image using OpenCV
-    target_image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
+    # Load the target image from the specified path
+    target_image = Image.open(image_path)
+
+    # Convert the target image to RGBA if needed
+    if target_image.mode != 'RGBA':
+        target_image = target_image.convert('RGBA')
+
+    # Convert the target image to ndarray
+    target_image = np.array(target_image)
 
     # Convert the target image to floating point representation in the range [0, 1]
     target_image = target_image.astype(np.float32) / 255.0
@@ -22,7 +41,7 @@ def load_target_image(image_path):
 
 def create_individual(num_triangles, image_width, image_height):
     # Create a random individual (chromosome)
-    # Each triangle has 3 vertices (6 coordinates) and 3 color values (RGBA)
+    # Each triangle has 3 vertices (6 coordinates) and 4 color values (RGBA)
     individual = []
 
     for i in range(num_triangles):
@@ -48,20 +67,43 @@ def initialize_population(population_size, num_triangles, image_width, image_hei
     return [create_individual(num_triangles, image_width, image_height) for _ in range(population_size)]
 
 
-def render_triangles(individual, image_width, image_height):
-    # Create an empty image
-    image = np.zeros((image_height, image_width, 3))
+# def render_triangles(individual, image_width, image_height):
+#     # Create an empty image
+#     image = np.zeros((image_height, image_width, 4))
+#
+#     # Iterate over the triangles
+#     for i in range(0, len(individual), 10):
+#         # Extract triangle vertices and color
+#         vertices = np.array([(individual[i]*image_width, individual[i + 1]*image_height),
+#                              (individual[i + 2]*image_width, individual[i + 3]*image_height),
+#                              (individual[i + 4]*image_width, individual[i + 5]*image_height)], dtype=np.int32)
+#         color = (individual[i + 6]*255, individual[i + 7]*255, individual[i + 8]*255, individual[i + 9]*255)
+#
+#         # Draw the triangle on the image
+#         cv2.fillConvexPoly(image, vertices, color, cv2.LINE_AA)
+#
+#     return image
 
-    # Iterate over the triangles
-    for i in range(0, len(individual), 10):
-        # Extract triangle vertices and color
-        vertices = np.array([(individual[i], individual[i + 1]), (individual[i + 2], individual[i + 3]),
-                             (individual[i + 4], individual[i + 5])], dtype=np.int32)
-        color = (individual[i + 6], individual[i + 7], individual[i + 8], individual[i + 9])
+
+def render_triangles(individual, width, height):
+    # Create a blank image with a white background
+    image = Image.new('RGBA', (width, height), (255, 255, 255, 255))
+
+    for i in range(int(len(individual)/10)):
+        # Extract the triangle's vertices and color (RGBA) values from the individual
+        x1, y1 = individual[i * 10 + 0] * width, individual[i * 10 + 1] * height
+        x2, y2 = individual[i * 10 + 2] * width, individual[i * 10 + 3] * height
+        x3, y3 = individual[i * 10 + 4] * width, individual[i * 10 + 5] * height
+        r, g, b, a = [int(value * 255) for value in individual[i * 10 + 6:i * 10 + 10]]
 
         # Draw the triangle on the image
-        cv2.fillConvexPoly(image, vertices, color)
+        triangle = Image.new('RGBA', (width, height), (0, 0, 0, 0))
+        draw = ImageDraw.Draw(triangle)
+        draw.polygon([(x1, y1), (x2, y2), (x3, y3)], fill=(r, g, b, a))
 
+        # Overlay the triangle onto the main image
+        image = Image.alpha_composite(image, triangle)
+    image = np.array(image).astype(np.float32) / 255.0
     return image
 
 
@@ -98,7 +140,7 @@ def selection(population, fitnesses):
 
 
 def crossover(parent1, parent2):
-    crossover_point = random.randrange(0, len(parent1), 10)
+    crossover_point = random.randint(1, len(parent1))
 
     # Perform single-point crossover
     child1 = parent1[:crossover_point] + parent2[crossover_point:]
@@ -127,34 +169,45 @@ def mutation(individual, mutation_rate, mutation_scale):
     return mutated_individual
 
 
+# def save_and_display_image(final_image, save_path):
+#     # Convert the image back to the range [0, 255] and convert to uint8
+#     final_image_uint8 = (final_image * 255).astype(np.uint8)
+#
+#     # Save the image using OpenCV
+#     cv2.imwrite(save_path, final_image_uint8)
+#
+#     # Display the image using OpenCV
+#     cv2.imshow('Final Image', final_image_uint8)
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
+
+
 def save_and_display_image(final_image, save_path):
     # Convert the image back to the range [0, 255] and convert to uint8
-    final_image_uint8 = (final_image * 255).astype(np.uint8)
+    final_image_uint8 = (final_image*255).astype(np.uint8)
+    final_image = Image.fromarray(np.uint8(final_image_uint8))
+    # Save the final image to the specified path
+    final_image.save(save_path)
 
-    # Save the image using OpenCV
-    cv2.imwrite(save_path, final_image_uint8)
-
-    # Display the image using OpenCV
-    cv2.imshow('Final Image', final_image_uint8)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # Display the final image
+    final_image.show()
 
 
 def main():
     # Load the target image
-    image_path = 'chrome.jpg'
+    image_path = 'chrome.png'
     target_image = load_target_image(image_path)
 
     # Initialize the population
     population_size = 100
-    num_triangles = 50
+    num_triangles = 256
     image_width = target_image.shape[1]
     image_height = target_image.shape[0]
 
     population = initialize_population(population_size, num_triangles, image_width, image_height)
 
     # Define GA parameters
-    generations = 500
+    generations = 200
     mutation_rate = 0.1
     mutation_scale = 0.05
 
@@ -186,9 +239,13 @@ def main():
     final_image = render_triangles(best_individual, image_width, image_height)
 
     # Save and display the final image
-    save_path = 'chromeFit.jpg'
+    save_path = 'chromeFit.png'
     save_and_display_image(final_image, save_path)
 
 
 if __name__ == '__main__':
+    import time
+    start = time.perf_counter()
     main()
+    end = time.perf_counter()
+    print(end-start)
