c8409a681fe451764d27e2178046c54a4e1c0ad7
Rvosuke <2409215698@qq.com>
2023-03-21 23:33:47 +0800

    commit
    3.21

--- imageFitting.py
+++ imageFitting.py
@@ -3,23 +3,12 @@ import random
 # import cv2
 from PIL import Image, ImageDraw
 
-
 # Parameters
 population_size = 20
 num_triangles = 128
 mutation_rate = 0.1
 crossover_rate = 0.8
 num_generations = 100
-
-
-# def load_target_image(image_path):
-#     # Load the target image using OpenCV
-#     target_image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
-#
-#     # Convert the target image to floating point representation in the range [0, 1]
-#     target_image = target_image.astype(np.float32) / 255.0
-#
-#     return target_image
 
 
 def load_target_image(image_path):
@@ -39,66 +28,101 @@ def load_target_image(image_path):
     return target_image
 
 
-def create_individual(num_triangles, image_width, image_height):
-    # Create a random individual (chromosome)
+# def create_individual(num_triangles, image_width, image_height):
+#     """Create a random individual (chromosome)"""
+#     # Each triangle has 3 vertices (6 coordinates) and 4 color values (RGBA)
+#     individual = []
+#
+#     for i in range(num_triangles):
+#         # Generate random triangle vertices
+#         for j in range(3):
+#             x = int(np.random.uniform(0, image_width + 1))
+#             y = int(np.random.uniform(0, image_height + 1))
+#             individual.extend([x, y])
+#
+#         # Generate random color values (RGBA)
+#         r = random.random()
+#         g = random.random()
+#         b = random.random()
+#         a = random.random()
+#         individual.extend([r, g, b, a])
+#
+#     return individual
+#     # return [random.random() for _ in range(9 * num_triangles)]
+# def create_individual(num_triangles, image_width, image_height, target_image):
+#     """Create a random individual (chromosome)"""
+#     # Each triangle has 3 vertices (6 coordinates) and 4 color values (RGBA)
+#     individual = []
+#
+#     for i in range(num_triangles):
+#         # Generate random triangle vertices
+#         for j in range(3):
+#             x = int(np.random.uniform(0, image_width + 1))
+#             y = int(np.random.uniform(0, image_height + 1))
+#             individual.extend([x, y])
+#
+#         # Generate random color values (RGBA) based on the target_image
+#         x = random.randint(0, image_width - 1)
+#         y = random.randint(0, image_height - 1)
+#         r, g, b, a = target_image[y, x, :]
+#
+#         individual.extend([r, g, b, a])
+#
+#     return individual
+def create_individual(num_triangles, image_width, image_height, target_image):
+    """Create a random individual (chromosome)"""
     # Each triangle has 3 vertices (6 coordinates) and 4 color values (RGBA)
     individual = []
 
     for i in range(num_triangles):
         # Generate random triangle vertices
         for j in range(3):
-            x = random.uniform(0, image_width)
-            y = random.uniform(0, image_height)
+            x = int(np.random.uniform(0, image_width + 1))
+            y = int(np.random.uniform(0, image_height + 1))
             individual.extend([x, y])
 
-        # Generate random color values (RGBA)
-        r = random.random()
-        g = random.random()
-        b = random.random()
-        a = random.random()
+        # Ensure the three vertices are different to form a triangle
+        while (individual[-6:-4] == individual[-4:-2]) or (individual[-6:-4] == individual[-2:]) or (individual[-4:-2] == individual[-2:]):
+            individual[-2] = int(np.random.uniform(0, image_width + 1))
+            individual[-1] = int(np.random.uniform(0, image_height + 1))
+
+        # Generate random color values (RGBA) based on the target_image
+        x = random.randint(0, image_width - 1)
+        y = random.randint(0, image_height - 1)
+        r, g, b, a = target_image[y, x, :]
+
         individual.extend([r, g, b, a])
 
     return individual
-    # return [random.random() for _ in range(9 * num_triangles)]
 
 
-def initialize_population(population_size, num_triangles, image_width, image_height):
+def initialize_population(population_size, num_triangles, image_width, image_height, target_image):
     # Initialize the population
-    return [create_individual(num_triangles, image_width, image_height) for _ in range(population_size)]
+    return [create_individual(num_triangles, image_width, image_height, target_image) for _ in range(population_size)]
 
 
-# def render_triangles(individual, image_width, image_height):
-#     # Create an empty image
-#     image = np.zeros((image_height, image_width, 4))
-#
-#     # Iterate over the triangles
-#     for i in range(0, len(individual), 10):
-#         # Extract triangle vertices and color
-#         vertices = np.array([(individual[i]*image_width, individual[i + 1]*image_height),
-#                              (individual[i + 2]*image_width, individual[i + 3]*image_height),
-#                              (individual[i + 4]*image_width, individual[i + 5]*image_height)], dtype=np.int32)
-#         color = (individual[i + 6]*255, individual[i + 7]*255, individual[i + 8]*255, individual[i + 9]*255)
-#
-#         # Draw the triangle on the image
-#         cv2.fillConvexPoly(image, vertices, color, cv2.LINE_AA)
-#
-#     return image
+def clip_point(point, width, height):
+    """检查和修正顶点坐标是否在画布范围内"""
+    x, y = point
+    x = max(min(x, width), 0)
+    y = max(min(y, height), 0)
+    return x, y
 
 
 def render_triangles(individual, width, height):
     # Create a blank image with a white background
-    image = Image.new('RGBA', (width, height), (255, 255, 255, 255))
+    image = Image.new('RGBA', (width, height), (255, 255, 255, 0))
 
     for i in range(int(len(individual)/10)):
         # Extract the triangle's vertices and color (RGBA) values from the individual
-        x1, y1 = individual[i * 10 + 0] * width, individual[i * 10 + 1] * height
-        x2, y2 = individual[i * 10 + 2] * width, individual[i * 10 + 3] * height
-        x3, y3 = individual[i * 10 + 4] * width, individual[i * 10 + 5] * height
+        x1, y1 = clip_point((individual[i * 10 + 0], individual[i * 10 + 1]), width, height)
+        x2, y2 = clip_point((individual[i * 10 + 2], individual[i * 10 + 3]), width, height)
+        x3, y3 = clip_point((individual[i * 10 + 4], individual[i * 10 + 5]), width, height)
         r, g, b, a = [int(value * 255) for value in individual[i * 10 + 6:i * 10 + 10]]
 
         # Draw the triangle on the image
         triangle = Image.new('RGBA', (width, height), (0, 0, 0, 0))
-        draw = ImageDraw.Draw(triangle)
+        draw = ImageDraw.Draw(triangle, 'RGBA')
         draw.polygon([(x1, y1), (x2, y2), (x3, y3)], fill=(r, g, b, a))
 
         # Overlay the triangle onto the main image
@@ -112,7 +136,7 @@ def fitness(individual, target_image):
     rendered_image = render_triangles(individual, target_image.shape[1], target_image.shape[0])
 
     # Compute the difference between the rendered image and target image
-    difference = np.mean((rendered_image - target_image) ** 2)
+    difference = np.mean(np.abs(rendered_image - target_image))
 
     # Define fitness as the negative difference (higher fitness means smaller difference)
     return -difference
@@ -169,22 +193,9 @@ def mutation(individual, mutation_rate, mutation_scale):
     return mutated_individual
 
 
-# def save_and_display_image(final_image, save_path):
-#     # Convert the image back to the range [0, 255] and convert to uint8
-#     final_image_uint8 = (final_image * 255).astype(np.uint8)
-#
-#     # Save the image using OpenCV
-#     cv2.imwrite(save_path, final_image_uint8)
-#
-#     # Display the image using OpenCV
-#     cv2.imshow('Final Image', final_image_uint8)
-#     cv2.waitKey(0)
-#     cv2.destroyAllWindows()
-
-
 def save_and_display_image(final_image, save_path):
     # Convert the image back to the range [0, 255] and convert to uint8
-    final_image_uint8 = (final_image*255).astype(np.uint8)
+    final_image_uint8 = (final_image * 255).astype(np.uint8)
     final_image = Image.fromarray(np.uint8(final_image_uint8))
     # Save the final image to the specified path
     final_image.save(save_path)
@@ -199,16 +210,16 @@ def main():
     target_image = load_target_image(image_path)
 
     # Initialize the population
-    population_size = 100
-    num_triangles = 256
+    population_size = 20
+    num_triangles = 128
     image_width = target_image.shape[1]
     image_height = target_image.shape[0]
 
-    population = initialize_population(population_size, num_triangles, image_width, image_height)
+    population = initialize_population(population_size, num_triangles, image_width, image_height, target_image)
 
     # Define GA parameters
-    generations = 200
-    mutation_rate = 0.1
+    generations = 50
+    mutation_rate = 0.3
     mutation_scale = 0.05
 
     # Run the genetic algorithm
@@ -245,7 +256,8 @@ def main():
 
 if __name__ == '__main__':
     import time
+
     start = time.perf_counter()
     main()
     end = time.perf_counter()
-    print(end-start)
+    print('Running time：', end - start, 's')
